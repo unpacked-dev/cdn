6381cc9182ffb27a2405cc9c17df641b3c331bbf
Fabian <63110902+unpacked-dev@users.noreply.github.com>
2023-05-17 14:14:42 +0200

    Added better css & js

---
+++ public/better.js
@@ -0,0 +1,17 @@
+function debounce(func, wait) {
+    let timeout;
+
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
+
+async function sleep(wait) {
+    return new Promise(resolve => setTimeout(resolve, wait));
+}
